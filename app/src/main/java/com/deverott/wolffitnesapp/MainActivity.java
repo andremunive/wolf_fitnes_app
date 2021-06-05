089b7f1547534d35b3bad2e7232c2fdac00b53fe
andremunive <44244443+andremunive@users.noreply.github.com>
2021-06-05 12:38:05 -0500

    Initial commit

---
+++ app/src/main/java/com/deverott/wolffitnesapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.deverott.wolffitnesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Variables
+    Animation topAnim, bottomAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        doFirts();
+
+    }
+
+    private void doFirts(){
+        //Animaciones
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+    }
+
+}
